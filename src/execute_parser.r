c4b40935d37735dfc4442f9789659e215e3aa560
Sefa Ozalp <sefabey@gmail.com>
2018-08-22 18:53:46 +0100

    update execute parser inputh path (to relative) add parsed csv file

--- src/execute_parser.r
+++ src/execute_parser.r
@@ -1,9 +1,10 @@
 library(tidyverse)
 source("src/parse_jsonl.r")
 
-input_path <- "/media/datalab3/data1/backup_dataset/post-election/jsonl"
+input_path <- "data/"
 # output_path <- "data/csvs/"
 files <- list.files(input_path,full.names = T)
 files
-parallel::mclapply(X = files[1:5], FUN = parse_jsonl, mc.cores=3)
+
+parallel::mclapply(X = files[2], FUN = parse_jsonl, mc.cores=3)
 
