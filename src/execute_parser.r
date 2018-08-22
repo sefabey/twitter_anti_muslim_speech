202ad24562c5ae753623a5c9fbb9363071058fd7
Sefa Ozalp <sefabey@gmail.com>
2018-08-22 19:03:23 +0100

    add safely to parser executer restructure data/

--- src/execute_parser.r
+++ src/execute_parser.r
@@ -1,10 +1,10 @@
 library(tidyverse)
 source("src/parse_jsonl.r")
 
-input_path <- "data/"
+input_path <- "data/test_jsonl"
 # output_path <- "data/csvs/"
 files <- list.files(input_path,full.names = T)
 files
 
-parallel::mclapply(X = files[2], FUN = parse_jsonl, mc.cores=3)
+parallel::mclapply(X = files, FUN = safely(parse_jsonl), mc.cores=3)
 
